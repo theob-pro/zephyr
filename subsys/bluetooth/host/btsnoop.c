5a107db97fe6ea422a5f05bc7efc32b2d7abf8e3
Théo Battrel <theo.battrel@nordicsemi.no>
2023-09-11 11:31:57 +0200

    add btsnoop over uart

--- subsys/bluetooth/host/btsnoop.c
+++ subsys/bluetooth/host/btsnoop.c
@@ -47,37 +47,29 @@ struct btsnoop_pkt_record {
 } __packed;
 
 static int btsnoop_file_fd;
+static const struct device *const btsnoop_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_bt_snoop_uart));
 
-static void btsnoop_write_hdr(void)
+static void btsnoop_open_file(void)
 {
-        struct btsnoop_hdr hdr;
-
-        memcpy(hdr.id_pattern, "btsnoop", 8);
-        hdr.version = sys_cpu_to_be32(1);
-        hdr.datalink_type = sys_cpu_to_be32(1001); /* Un-encapsulated HCI (H1). The type of packet is indicated by the flag set. */
-
-        if (write(btsnoop_file_fd, &hdr, sizeof(hdr)) != sizeof(hdr)) {
-		close(btsnoop_file_fd);
-                return;
-        }
-}
-
-static void btsnoop_init(void)
-{
-	LOG_ERR("%s", __func__);
-        pid_t pid = getpid(); // max value 2^22-1, 7 digits
+	pid_t pid = getpid(); // max value 2^22-1, 7 digits
 	char filename[BTSNOOP_FILENAME_LEN]; // log.PID.btsnoop
 
 	snprintk(filename, BTSNOOP_FILENAME_LEN, "log.%d.btsnoop", pid);
-	printk("filename: %s\n", filename);
 
 	btsnoop_file_fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, (mode_t)0666);
 	if (btsnoop_file_fd == -1) {
 		k_oops();
 		return;
 	}
+}
 
-        btsnoop_write_hdr();
+static void btsnoop_uart_send(const void *data, size_t len)
+{
+	const uint8_t *buf = data;
+
+	while (len--) {
+		uart_poll_out(btsnoop_dev, *buf++);
+        }
 }
 
 static uint64_t btsnoop_ts_get(void)
@@ -93,6 +85,26 @@ static uint64_t btsnoop_ts_get(void)
 	return k_cyc_to_us_floor64(cycles) + 0x00E03AB44A676000;
 }
 
+static void btsnoop_write(struct btsnoop_pkt_record pkt, const void *data, size_t len)
+{
+	IF_ENABLED(CONFIG_BT_BTSBOOP_FILE, ({
+                if (write(btsnoop_file_fd, &pkt, sizeof(pkt)) == -1) {
+                        close(btsnoop_file_fd);
+                        return;
+                }
+
+                if (write(btsnoop_file_fd, data, len) == -1) {
+                        close(btsnoop_file_fd);
+                        return;
+                }
+        }))
+
+	IF_ENABLED(CONFIG_BT_BTSNOOP_UART, ({
+		btsnoop_uart_send(&pkt, sizeof(pkt));
+		btsnoop_uart_send(data, len);
+        }))
+}
+
 void bt_btsnoop_write(enum bt_btsnoop_flag flag, const void *data, size_t len)
 {
         struct btsnoop_pkt_record pkt;
@@ -106,15 +118,45 @@ void bt_btsnoop_write(enum bt_btsnoop_flag flag, const void *data, size_t len)
 	LOG_ERR("%s", __func__);
 	LOG_HEXDUMP_DBG(data, len, "data:");
 
-        if (write(btsnoop_file_fd, &pkt, sizeof(pkt)) == -1) {
-                close(btsnoop_file_fd);
-                return;
-        }
+        btsnoop_write(pkt, data, len);
+}
 
-	if (write(btsnoop_file_fd, data, len) == -1) {
-		close(btsnoop_file_fd);
-		return;
-	}
+static void btsnoop_write_hdr(void)
+{
+        struct btsnoop_hdr hdr;
+
+        memcpy(hdr.id_pattern, "btsnoop", 8);
+        hdr.version = sys_cpu_to_be32(1);
+        hdr.datalink_type = sys_cpu_to_be32(1001); /* Un-encapsulated HCI (H1). The type of packet is indicated by the flag set. */
+
+	IF_ENABLED(CONFIG_BT_BTSNOOP_FILE, ({
+                if (write(btsnoop_file_fd, &hdr, sizeof(hdr)) != sizeof(hdr)) {
+                        close(btsnoop_file_fd);
+                        return;
+                }
+	}))
+
+	IF_ENABLED(CONFIG_BT_BTSNOOP_UART, ({
+                btsnoop_uart_send(&hdr, sizeof(hdr));
+        }))
+}
+
+static void btsnoop_init(void)
+{
+	IF_ENABLED(CONFIG_BT_BTSNOOP_FILE, ({
+		btsnoop_open_file();
+        }))
+
+	IF_ENABLED(CONFIG_BT_BTSNOOP_UART, ({
+                __ASSERT_NO_MSG(device_is_ready(btsnoop_dev));
+
+		IF_ENABLED(CONFIG_UART_INTERRUPT_DRIVEN, ({
+			uart_irq_rx_disable(btsnoop_dev);
+			uart_irq_tx_disable(btsnoop_dev);
+                }))
+        }))
+
+        btsnoop_write_hdr();
 }
 
 static int bt_btsnoop_init()
