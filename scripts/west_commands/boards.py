daeec35004539840fd836cb7f0722ef935248332
Théo Battrel <theo.util@protonmail.ch>
2024-03-12 21:55:01 +0100

    west: add vendor to boards format
    Signed-off-by: Théo Battrel <theo.util@protonmail.ch>

--- scripts/west_commands/boards.py
+++ scripts/west_commands/boards.py
@@ -93,5 +93,12 @@ class Boards(WestCommand):
         for board in list_boards.find_v2_boards(args):
             if name_re is not None and not name_re.search(board.name):
                 continue
-            log.inf(args.format.format(name=board.name, arch='', dir=board.dir, hwm=board.hwm,
-                                       qualifiers=list_boards.board_v2_qualifiers_csv(board)))
+            log.inf(
+                args.format.format(
+                    name=board.name,
+                    dir=board.dir,
+                    hwm=board.hwm,
+                    vendor=board.vendor,
+                    qualifiers=list_boards.board_v2_qualifiers_csv(board),
+                )
+            )
